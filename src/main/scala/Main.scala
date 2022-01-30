a37fd4d622fcd5ec44f59ee1eeddf9313dc98dce
Misza <mniester@gmail.com>
2022-01-30 12:12:27 +0100

    Jsoup works and tested

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,2 +1,6 @@
+import org.jsoup.Jsoup
+
 @main def main(args: String*): Unit = 
   val lang = if (args.length != 2) then "en" else args(0).toLowerCase()
+  val doc = Jsoup.connect("http://en.wikipedia.org/").get()
+  println(doc.title())
--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -1,5 +1,7 @@
 import org.scalatest.funsuite.AnyFunSuite
+import org.jsoup.Jsoup
 
 
 class TestSuite extends AnyFunSuite:
-  test("test ScalaTest") (assert (true == true))
+  test("test ScalaTest") {assert (true == true)}
+  test("Jsoup scrapping") {assert(Jsoup.connect("http://en.wikipedia.org/").get().title() == "Wikipedia, the free encyclopedia")}
