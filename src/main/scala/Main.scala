1b2947cc923f5e0bc2ce78875983ee3e4435ef47
Misza <mniester@gmail.com>
2022-01-30 13:24:27 +0100

    IO Singleton with firts method - file reading

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,15 @@
 import org.jsoup.Jsoup
+import scala.io.Source
 
 @main def main(args: String*): Unit = 
   val lang = if (args.length != 2) then "en" else args(0).toLowerCase()
 
+  
+trait FileReader:
+  def readInput(fileName: String): Iterator[String] = 
+    for 
+      code <- Source.fromFile(fileName).getLines()
+    yield
+      code
+
+object IOSingleton extends FileReader
--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -5,5 +5,11 @@ import scala.io.Source
 
 class TestSuite extends AnyFunSuite:
   test("test ScalaTest") {assert (true == true)}
-  test("Jsoup scrapping") {assert(Jsoup.connect("http://en.wikipedia.org/").get().title() == "Wikipedia, the free encyclopedia")}
-  test("Code Reader") {val lines = Source.fromFile("fakemoviecodes.txt").getLines.toList == List("aaa", "bbb", "ccc")}
+  // test("Jsoup scrapping") {assert(Jsoup.connect("http://en.wikipedia.org/").get().title() == "Wikipedia, the free encyclopedia")}
+  test("Code Reader")
+    {
+    for 
+      (code, sample) <- IOSingleton.readInput("codes.txt").zip(List("aaa", "bbb", "ccc"))
+    do
+      assert(code == sample)
+    }
