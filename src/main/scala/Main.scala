06a7e6f28e97ca05bce07b74b8e3f0f35528a706
Misza <mniester@gmail.com>
2022-01-30 12:30:22 +0100

    test of file reading

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,5 +2,4 @@ import org.jsoup.Jsoup
 
 @main def main(args: String*): Unit = 
   val lang = if (args.length != 2) then "en" else args(0).toLowerCase()
-  val doc = Jsoup.connect("http://en.wikipedia.org/").get()
-  println(doc.title())
+
--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -1,7 +1,9 @@
 import org.scalatest.funsuite.AnyFunSuite
 import org.jsoup.Jsoup
+import scala.io.Source
 
 
 class TestSuite extends AnyFunSuite:
   test("test ScalaTest") {assert (true == true)}
   test("Jsoup scrapping") {assert(Jsoup.connect("http://en.wikipedia.org/").get().title() == "Wikipedia, the free encyclopedia")}
+  test("Code Reader") {val lines = Source.fromFile("fakemoviecodes.txt").getLines.toList == List("aaa", "bbb", "ccc")}
