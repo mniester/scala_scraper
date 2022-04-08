87244b7ee3a8ec04896d0d8009275208e2a82f9f
Misza <mniester@gmail.com>
2022-04-09 00:47:38 +0200

    Task Factory

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -3,5 +3,5 @@ package Config
 object CommonSetings {
     val maxUserNameLength = 32
     val maxProjectNameLength = 32
-
+    val maxTaskCommentLength = 256
 }
--- src/main/scala/Inputs.scala
+++ src/main/scala/Inputs.scala
@@ -8,7 +8,7 @@ case class User(name: String)
 
 case class Project(name: String, userName: String, startTime: String)
 
-case class Task(start: String, projec: String, time: Int,  volume: Option[Int], comment: Option[String])
+case class Task(start: String, project: String, time: Int,  volume: Option[Int], comment: Option[String])
 
 
 object CheckISOTimeFormat {
@@ -25,11 +25,19 @@ object UserFactory {
     }
 }
 
-
 object ProjectFactory {
   def apply (name: String, userName: String, startTime: String): Option[Project] =
-    if (name.length <= CommonSetings.maxProjectNameLength || userName.length <= CommonSetings.maxUserNameLength || CheckISOTimeFormat(startTime)) {
+    if ((name.length <= CommonSetings.maxProjectNameLength) || (userName.length <= CommonSetings.maxUserNameLength) || CheckISOTimeFormat(startTime)) {
       Some(Project(name, userName, startTime))
+    } else {
+      None
+    }
+}
+
+object TaskFactory {
+  def apply (start: String, project: String, time: Int,  volume: Option[Int], comment: Option[String]): Option[Task] =
+    if ((!comment.isEmpty) && (comment.head.length <= CommonSetings.maxTaskCommentLength)) {
+      Some(Task(start, project, time,  volume, comment))
     } else {
       None
     }
